85276c51f2b773ea4b5565fc6cf187428d058d98
lsy <lsy22@hotmail.com>
2024-06-17 21:29:24 +0800

    更新docker专用备份脚本

---
+++ shell/code/webbackups_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+web_path="/var/www" # 数据路径
+date_time=$(date +"%Y_%m_%d") # 日期格式
+year=$(date +"%Y") #年份
+original_dir=$(pwd) # 记录原始目录
+
+for item in "$web_path"/*; do
+    item_name=$(basename "$item")
+    # 切换到网站目录进行压缩
+    cd "$item" || exit
+    zip -r "${item_name}_${date_time}.zip" .
+    # 移动压缩文件回原始目录
+    mv "${item_name}_${date_time}.zip" "$original_dir"
+    # 返回原始目录
+    cd "$original_dir" || exit
+    # 激活百度网盘环境
+    source ~/shell/myvenv/bin/activate
+    # 上传到云存储
+    bypy upload "${item_name}_${date_time}.zip" "/${item_name}/"
+    # 上传到阿里云盘
+    aliyunpan upload "${item_name}_${date_time}.zip" "/网站/${item_name}/${year}/"
+    # 删除文件
+    rm "${item_name}_${date_time}.zip"
+done
